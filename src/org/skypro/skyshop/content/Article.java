0deed5f591b37cb0018247370714d0cb250eb462
MarkovskyKot <0postaldude0@gmail.com>
2025-08-11 19:55:57 +0700

    Homework on the topic Set completed

--- src/org/skypro/skyshop/content/Article.java
+++ src/org/skypro/skyshop/content/Article.java
@@ -2,6 +2,8 @@ package org.skypro.skyshop.content;
 
 import org.skypro.skyshop.search.Searchable;
 
+import java.util.Objects;
+
 public final class Article implements Searchable {
     private final String title;
     private final String text;
@@ -29,5 +31,18 @@ public final class Article implements Searchable {
     @Override
     public String toString() {
         return title + "\n" + text + "\n";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Article article = (Article) o;
+        return Objects.equals(title.toLowerCase(), article.title.toLowerCase());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(title.toLowerCase());
     }
 }
--- src/org/skypro/skyshop/search/SearchEngine.java
+++ src/org/skypro/skyshop/search/SearchEngine.java
@@ -5,32 +5,38 @@ import org.skypro.skyshop.exceptions.BestResultNotFound;
 import java.util.*;
 
 public class SearchEngine {
-    private final List<Searchable> searchableList = new LinkedList<>();
+    private final Set<Searchable> searchableSet = new HashSet<>();
+    private static final Comparator<Searchable> LENGTH_THEN_NATURAL_ORDER= (s1, s2) -> {
+        int lengthCompare = Integer.compare(s2.getName().length(), s1.getName().length());
+        return lengthCompare != 0 ? lengthCompare : s1.getName().compareTo(s2.getName());
+    };
 
     public void add(Searchable item) {
-        searchableList.add(item);
-        System.out.println("*Объект добавлен*\n");
+        if (item != null) {
+            searchableSet.add(item);
+            System.out.println("*Объект добавлен*\n");
+        }
     }
 
-    public Map<String, Searchable> search(String query) {
-        Map<String, Searchable> resultMap = new TreeMap<>();
+    public Set<Searchable> search(String query) {
+        Set<Searchable> resultMap = new TreeSet<>(LENGTH_THEN_NATURAL_ORDER);
 
         if (query == null || query.isEmpty()) {
             return resultMap;
         }
         String queryLower = query.toLowerCase();
-        for (Searchable item : searchableList) {
-            if (item != null && item.getSearchTerm() != null && item.getSearchTerm().toLowerCase().contains(queryLower)) {
-                if (!resultMap.containsKey(item.getName())) {
-                    resultMap.put(item.getName(), item);
-                }
+        for (Searchable item : searchableSet) {
+            if (item != null
+                    && item.getSearchTerm() != null
+                    && item.getSearchTerm().toLowerCase().contains(queryLower)) {
+                    resultMap.add(item);
             }
         }
         return resultMap;
     }
 
     public Searchable getBestSearchable(String search) throws BestResultNotFound {
-        if (search == null || search.isEmpty() || searchableList.isEmpty()) {
+        if (search == null || search.isEmpty() || searchableSet.isEmpty()) {
             throw new BestResultNotFound(search);
         }
 
@@ -38,7 +44,7 @@ public class SearchEngine {
         int maxCount = 0;
         String searchLower = search.toLowerCase();
 
-        for (Searchable s : searchableList) {
+        for (Searchable s : searchableSet) {
             if (s == null || s.getSearchTerm() == null) {
                 continue;
             }
